f7f882a8827a50472f923d95a8332766c56bd097
Jorge Herran <jaherran@PF1EDPTB.localdomain>
2022-03-02 22:24:44 -0500

    Changing application

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react 2
+          Learn react 3
         </a>
       </header>
     </div>
