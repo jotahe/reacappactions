ce8f64db04f4c59f4474188d88b610bc8842b218
Jorge Herran <jaherran@PF1EDPTB.localdomain>
2022-03-03 18:42:02 -0500

    make test to fail

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react 6 Testing Github Actions
+          Learn
         </a>
       </header>
     </div>
