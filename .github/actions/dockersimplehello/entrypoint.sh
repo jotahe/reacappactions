af68309cf1c60076f7e9e65bc9c355959db2aa0a
Jorge Herran <jaherran@PF1EDPTB.localdomain>
2022-03-05 08:08:16 -0500

    Test dockersimplehello action

---
+++ .github/actions/dockersimplehello/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+# to log and debug message we can do this
+echo "::debug ::Debug Message"   # VM would now that it have to load a debug message
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"   # We mask the output so it is not shown on the screen
+echo "HI $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo 'some stuff'
+echo 'some stuff'
+echo 'some stuff'
+echo "::endgroup"
+
+echo '::set-env name:DOCKERSIMPLEHELLO::dockersimplehello'   # Set environment variable
