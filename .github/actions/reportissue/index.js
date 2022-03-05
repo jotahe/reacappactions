736bb375cbbb805cfb34d5db823dfab22d8266c6
Jorge Herran <jaherran@PF1EDPTB.localdomain>
2022-03-05 07:08:29 -0500

    Add a reportissue own action

---
+++ .github/actions/reportissue/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const token = core.getInput('token');
+  const title = core.getInput('title');
+  const body = core.getInput('body');
+  const assignees = core.getInput('assignees');
+
+  // fetch we can use a fetch function to send api requests but github core have a package to do it
+  const oktokit = new github.GitHub(token);
+  
+  const response = octokit.issue.create({
+      // owner: github.context.repo.owner,   // We get the token directly from github vbles
+      // repo: github.context.repo.,
+      ...github.context.repo,    // we can use this other form to pass context
+      //title: title, // in javascript when the name of vble match assignment you can omit one of those
+      title,
+      body,
+      assignees: assignees ? assignees.split(',') : undefined  // it would make the string into array
+      // assignees: assignees ? assignees.split('\n') : undefined  // in this case we would fill the array separating by lines
+   });
+   core.setOutput("issue", JSON.stringify(response,data));  // it would be a string a not an object
+
+
+} catch(error) {
+    core.setFailed(error.message);
+}
